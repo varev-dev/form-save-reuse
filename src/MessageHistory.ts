d42e6559a20f1e60d5324822b67ccb194317d798
Kacper Doga <doga.kacper@gmail.com>
2023-11-08 10:56:13 +0100

    reload message history on save/select

--- src/MessageHistory.ts
+++ src/MessageHistory.ts
@@ -1,17 +1,21 @@
 const sentButton = document.getElementById('toggle-sent') as HTMLButtonElement;
 const notSentButton = document.getElementById('toggle-not-sent') as HTMLButtonElement;
 
-const sentList = document.querySelector('.message-sent') as HTMLDivElement;
-const notSentList = document.querySelector('.message-not-sent') as HTMLDivElement;
+let sentList = document.querySelector('.message-sent') as HTMLDivElement;
+let notSentList = document.querySelector('.message-not-sent') as HTMLDivElement;
 
 const messageHistory = document.querySelector('.messages') as HTMLDivElement;
 const imageHistory = document.querySelector('.contact-history img') as HTMLDivElement;
 
 const showHistoryListIfNotEmpty = () => {
-    if (sentList.hasChildNodes() || notSentList.hasChildNodes()) {
+    if (sentList.childNodes.length > 0 || notSentList.childNodes.length > 0) {
         messageHistory.style.display = "flex";
         imageHistory.style.display = "none";
+        return;
     }
+    messageHistory.style.display = "none";
+    imageHistory.style.display = "block";
+    return;
 }
 
 const generateMessages = (status: MessageStatus) => {
@@ -43,6 +47,12 @@ const createMessageDiv = (message: Message, parent: HTMLDivElement) => {
     messageDivs.push(messageRoot);
 }
 
+const removeMessageDivs = (messageDivs: HTMLDivElement[]) => {
+    messageDivs.forEach(message => {
+        message.remove();
+    });
+}
+
 const chooseMessageFromHistory = (messageId: number) => {
     if (messageId >= messages.length)
         return;
@@ -62,4 +72,47 @@ const toggleClass = (button: HTMLButtonElement, className: string) => {
     button?.addEventListener('click', e => {
         button.parentElement?.classList.toggle(className);
     });
+}
+
+const loadMessagesFromLocalStorage = () => {
+    let array = localStorage.getItem('messages') as string;
+    messages = [];
+
+    try {
+        let message  = new Message("", "", ContactType.PRIVATE, MessageCategory.OTHER,
+            "", "", MessageStatus.NOT_SENT);
+
+        JSON.parse(array, (key, value) => {
+            switch (key) {
+                case "_fullName": message.fullName = value; break;
+                case "_emailAddress": message.emailAddress = value; break;
+                case "_contactType": message = setEnumValueInMessage(message, value, "ContactType"); break;
+                case "_category": message = setEnumValueInMessage(message, value, "MessageCategory"); break;
+                case "_title": message.title = value; break;
+                case "_content": message.content = value; break;
+                case "_status": message = setEnumValueInMessage(message, value, "MessageStatus"); break;
+                default:
+                    messages.push(message);
+                    message  = new Message("", "", ContactType.PRIVATE, MessageCategory.OTHER,
+                        "", "", MessageStatus.NOT_SENT);
+                    break;
+            }
+        });
+    } catch (e) {}
+}
+
+const initLocalStorageIfNull = () => {
+    if (localStorage.getItem('messages') == null)
+        localStorage.setItem('messages', '');
+}
+
+const loadAndPrintMessages = () => {
+    initLocalStorageIfNull();
+    loadMessagesFromLocalStorage();
+    removeMessageDivs(messageDivs);
+    sentList = document.querySelector('.message-sent') as HTMLDivElement;
+    notSentList = document.querySelector('.message-not-sent') as HTMLDivElement;
+    generateMessages(MessageStatus.NOT_SENT);
+    generateMessages(MessageStatus.SENT);
+    showHistoryListIfNotEmpty();
 }
