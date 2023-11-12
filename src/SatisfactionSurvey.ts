5c6b2bc343dc48d1b1055bcde9060b9ffb8e4ae6
Kacper Doga <doga.kacper@gmail.com>
2023-11-12 21:44:48 +0100

    create satisfaction survey

---
+++ src/SatisfactionSurvey.ts
@@ -0,0 +1,28 @@
+const dialog = $("#dialog");
+
+$( function() {
+    let max = $("#slider-range-max");
+    max.slider({
+        range: "max",
+        min: 1,
+        max: 5,
+        value: 1,
+        slide: function( event, ui ) {
+            ui.value ? $("#amount").val(ui.value) : "";
+        }
+    });
+    $("#amount").val(max.slider( "value" ));
+} );
+
+$( function() {
+    dialog.dialog({
+        width: "auto",
+        autoOpen: true,
+        modal: true,
+        buttons: {
+            Rate: function() {
+                $( this ).dialog( "close" );
+            }
+        }
+    });
+});
